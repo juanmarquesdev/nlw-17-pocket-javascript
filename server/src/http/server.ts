3fdd136066bcfddad37f5632d29a183940f3c4c8
juanmarquesdev <juanbatistamarques+github@gmail.com>
2024-09-13 00:04:24 -0300

    finishing server and starting web

---
+++ server/src/http/server.ts
@@ -0,0 +1,31 @@
+import fastifyCors from '@fastify/cors'
+import fastify from 'fastify'
+import {
+  type ZodTypeProvider,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { createCompletionRoute } from './routes/create-completion'
+import { createGoalRoute } from './routes/create-goal'
+import { getPendingGoalsRoute } from './routes/get-pending-goals'
+import { getWeekSummaryRoute } from './routes/get-week-summary'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(createGoalRoute)
+app.register(createCompletionRoute)
+app.register(getPendingGoalsRoute)
+app.register(getWeekSummaryRoute)
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => console.log('HTTP server running!'))
