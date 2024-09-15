a4812e5ddc49ea9d27ad1d742b9d8c690f2e8842
juanmarquesdev <juanbatistamarques+github@gmail.com>
2024-09-15 10:19:59 -0300

    update

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,31 @@
+import { Plus } from 'lucide-react'
+
+import letsStart from './assets/lets-start-illustration.svg'
+import logo from './assets/logo-in-orbit.svg'
+import { CreateGoal } from './components/create-goal'
+import { Button } from './ui/button'
+import { Dialog, DialogTrigger } from './ui/dialog'
+
 export function App() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-8">
-      
-    </div>
+    <Dialog>
+      <div className="h-screen flex flex-col items-center justify-center gap-8">
+        <img src={logo} alt="in.orbit" />
+        <img src={letsStart} alt="in.orbit" />
+        <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+          Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora
+          mesmo?
+        </p>
+
+        <DialogTrigger asChild>
+          <Button>
+            <Plus className="size-4" />
+            Cadastrar meta
+          </Button>
+        </DialogTrigger>
+      </div>
+
+      <CreateGoal />
+    </Dialog>
   )
 }
